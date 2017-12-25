57f1681efbfc27defc3dfc680bd988dea562bc8a
kevinmartinez <kevin@pixelkojan.se>
2017-12-25 02:52:54 +0100

    try to move inline js WIP

---
+++ views/socket.js
@@ -0,0 +1,26 @@
+import io from 'socket.io-client'
+
+const socket = io.connect('http://localhost:3030')
+const bot = document.querySelector('.chat-field__box--cpu p')
+const user = document.querySelector('.chat-field__box--user p')
+const userInput = document.querySelector('.msg__input')
+
+const btnSubmit = document.querySelector('.msg__btn.btn')
+console.log(bot)
+socket.on('hello', function (data) {
+  bot.textContent = data.message
+  console.log(data)
+  socket.emit('my other event', { my: 'data' })
+  btnSubmit.addEventListener('click', function (event) {
+    event.preventDefault()
+    const input = userInput.value
+    user.textContent = input
+    socket.emit('event from input', {message: input})
+    console.log(userInput.value)
+  })
+})
+
+socket.on('response', (data) => {
+  console.log(data)
+  bot.textContent = data.message
+})
